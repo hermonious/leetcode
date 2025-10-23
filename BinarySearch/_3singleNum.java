e1b94b7f0c80cbc7cce302bcff73262b6e485b94
Hermonious <92699508+hermonious@users.noreply.github.com>
2025-10-24 02:07:18 +0800

    Update _3singleNum.java

--- BinarySearch/_3singleNum.java
+++ BinarySearch/_3singleNum.java
@@ -4,30 +4,27 @@
 
 // 在一个排序的数组中，除一个数字只出现一次之外，其他数字都出现了两次，请找出这个唯一只出现一次的数字。
 // 例如，在数组[1, 1, 2, 2, 3, 4, 4, 5, 5]中，数字 3 只出现了一次
-
-
-public class _3singleNum {
-    
-
-    public int singleNum(int[] nums) {
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
         
         int left = 0;
-        int right = nums.length / 2;
+        int right = nums.length - 1;
 
-        while (left <= right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
 
-            int i = mid * 2;
-            
-            if (i < nums.length - 1 && nums[i] != nums[i + 1]) {
-                if (mid == 0 || nums[i - 2] > nums[i - 1]) {
-                    return nums[i];
-                }
-                right = mid - 1;
+            // 将 mid 调整为偶数，确保其指向成对元素的第一个位置
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] == nums[mid + 1]) {
+                // 左侧无异常，唯一元素在右半部分
+                left = mid + 2;
             } else {
-                left = mid + 1;
+                // 唯一元素在左半部分（包括 mid）
+                right = mid;
             }
         }
-        return nums[nums.length - 1];
+        return nums[left];
     }
 }
