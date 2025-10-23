bc434de1d3778459df8325ba3a3a73a605e3cc9a
Hermonious <92699508+hermonious@users.noreply.github.com>
2025-10-24 02:26:49 +0800

    Implement binary search algorithm in bs.java

--- BinarySearch/bs.java
+++ BinarySearch/bs.java
@@ -2,16 +2,17 @@
 
 // 基本模版
 public class bs {
-    
-
     public int search(int[] nums, int target) {
+        
         int left = 0;
         int right = nums.length - 1;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
+            
             if (nums[mid] == target) {
                 return mid;
+                
             } else if (nums[mid] < target) {
                 left = mid + 1;
             } else {
@@ -20,5 +21,4 @@ public class bs {
         }
         return -1;
     }
-
 }
