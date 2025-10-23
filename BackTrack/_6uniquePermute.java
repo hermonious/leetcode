3e3e943930c7d4f677cc9e7f224d87952eba3051
Hermonious <92699508+hermonious@users.noreply.github.com>
2025-10-24 01:27:17 +0800

    Update _6uniquePermute.java

--- BackTrack/_6uniquePermute.java
+++ BackTrack/_6uniquePermute.java
@@ -1,16 +1,14 @@
+
+// 给定一个包含重复数字的集合，请找出它的所有全排列。
+// 例如，集合[1, 1, 2]有 3 个全排列，分别是[1, 1, 2]、[1, 2, 1]和[2, 1, 1]
+
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-
 public class _6uniquePermute{
-
-
-
-    // 给定一个包含重复数字的集合，请找出它的所有全排列。
-    // 例如，集合[1, 1, 2]有 3 个全排列，分别是[1, 1, 2]、[1, 2, 1]和[2, 1, 1]
-    
     public List<List<Integer>> permute(int[] nums) {
 
         List<List<Integer>> res = new ArrayList<>();
@@ -19,18 +17,17 @@ public class _6uniquePermute{
         return res;
     }
 
-
     private void helper(int[] nums, int i, List<List<Integer>> res) {
+        
         if (i == nums.length) {
             List<Integer> permutation = new ArrayList<>();
 
             for (int num : nums) {
                 permutation.add(num);
             }
-
             res.add(permutation);
+            
         } else {
-
             // 使用了一个 HashSet，用来保存已经交换到排列下标为 i 的位置的所有值。只有当一个数值之前没有被交换到第 i 位时才做交换，否则直接跳过。 
             Set<Integer> set = new HashSet<>();
 
@@ -45,8 +42,6 @@ public class _6uniquePermute{
             }
         }
     }
-
-
 
     private void swap(int[] nums, int i, int j) {
         if(i != j) {
