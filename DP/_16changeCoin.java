924265df2c8ed5ce1eef648b58500ecb6efad0e7
Hermonious <92699508+hermonious@users.noreply.github.com>
2025-10-24 12:07:51 +0800

    Update _16changeCoin.java

--- DP/_16changeCoin.java
+++ DP/_16changeCoin.java
@@ -5,43 +5,36 @@
 
 // 给定正整数数组 coins 表示硬币的面额和一个目标总额 t，请计算凑出总额 t 至少需要的硬币数目。
 // 每种硬币可以使用任意多枚。如果不能用输入的硬币凑出给定的总额，则返回-1。
-// 例如，如果硬币的面额为[1, 3, 9, 10]，总额 t 为 15，那么至少需要 3 枚硬币，即 2 枚面额为 3 的硬币及 1 枚面额为 9 的硬币
+// 例如，如果硬币的面额为[1, 3, 9, 10]，总额 t 为 15，那么至少需要 3 枚硬币------- 2枚面额为 3 的硬币 +  1枚面额为 9 的硬币
 
 
 
+// f(i, j) = min(f(i-1, j - K * coins[i-1]) + K)
 
+// 其中，0 <= K <= j / coins[i-1]
+// 边界条件：f(0, j) = 0, j >= 0
+// f(i, 0) = 0, i >= 0
+// 目标：f(n, t)
 import java.util.Arrays;
 
-public class _16changeCoin {
-    
-
-
-
-    // f(i, j) = min(f(i-1, j - K * coins[i-1]) + K)
-
-    // 其中，0 <= K <= j / coins[i-1]
-    // 边界条件：f(0, j) = 0, j >= 0
-    // f(i, 0) = 0, i >= 0
-    // 目标：f(n, t)
-
-
-    public int coin(int[] coins, int target) {
-
-        int[] dp = new int[target + 1];
-        Arrays.fill(dp, target + 1);
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
         dp[0] = 0;
 
-
         for (int coin : coins) {
-            for (int j = target; j >= 1; j--) {
+            for (int j = amount; j >= 1; j--) {
                 for (int k = 1; k * coin <= j; k++) {
                     dp[j] = Math.min(dp[j], dp[j - k * coin] + k);
                 }
             }
         }
 
-        return dp[target] > target ? -1 : dp[target];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
+}
 
 
 
@@ -53,11 +46,11 @@ public class _16changeCoin {
     // coins[j] <= i
     // 边界条件：f(0) = 0
 
+    // 优化代码
     public int coin2(int[] coins, int target) {
 
         int[] dp = new int[target + 1];
         
-
         for (int i = 1; i <= target; i++) {
             dp[i] = target + 1;
             for (int coin : coins) {
@@ -69,7 +62,7 @@ public class _16changeCoin {
 
         return dp[target] > target ? -1 : dp[target];
     }
-}
+
 
 
 
