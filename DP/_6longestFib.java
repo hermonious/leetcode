7668c13b04b4935bb69a4b867f632c0dca7abffc
Hermonious <92699508+hermonious@users.noreply.github.com>
2025-10-24 12:33:55 +0800

    Update _6longestFib.java

--- DP/_6longestFib.java
+++ DP/_6longestFib.java
@@ -1,11 +1,9 @@
-import java.util.HashMap;
-import java.util.Map;
+
 
 
 
 // 输入一个没有重复数字的单调递增的数组，数组中至少有 3 个数字，请问数组中最长的斐波那契数列的长度是多少？
 // 例如，如果输入的数组是[1, 2, 3, 4, 5, 6, 7, 8]，由于其中最长的斐波那契数列是 1、2、3、5、8，因此输出是 5
-
 
 
 
@@ -19,6 +17,8 @@ import java.util.Map;
 
 // 需要一个二维数组来缓存 f(i, j)的计算结果。i 对应二维数组的行号，j 对应二维数组的列号。
 // 由于 i 大于j，因此实际上只用到了二维数组的左下角部分。如果数组的长度是 n，那么 i 的取值范围为 1～n-1，而 j 的取值范围为 0～n-2
+import java.util.HashMap;
+import java.util.Map;
 
 public class _6longestFib {
     public int fib(int[] A) {
