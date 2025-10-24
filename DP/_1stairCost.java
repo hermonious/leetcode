5011796eb9d2e51082c5664329abc5e6cfb96a39
Hermonious <92699508+hermonious@users.noreply.github.com>
2025-10-24 12:17:45 +0800

    Update _1stairCost.java

--- DP/_1stairCost.java
+++ DP/_1stairCost.java
@@ -2,15 +2,15 @@
 
 // 一个数组 cost 的所有数字都是正数，它的第 i 个数字表示在一个楼梯的第 i 级台阶往上爬的成本，在支付了成本 cost[i]之后可以从第 i 级台阶往上爬 1 级或 2 级。
 // 假设台阶至少有 2 级，既可以从第 0 级台阶出发，也可以从第 1 级台阶出发，请计算爬上该楼梯的最少成本。
-// 例如，输入数组[1, 100, 1, 1, 100, 1]，则爬上该楼梯的最少成本是 4，分别经过下标为 0、2、3、5 的这 4 级台阶
+// 例如，输入数组[1, 100, 1, 1, 100, 1]，则爬上该楼梯的最少成本是 4-------分别经过下标为 0、2、3、5 的这 4 级台阶
+
+
+// 状态转移方程表示为f(i) = min(f(i-1), f(i-2)) + cost[i]
+
 public class _1stairCost {
 
-
-    
-    // 状态转移方程表示为f(i) = min(f(i-1), f(i-2)) + cost[i]
-    // 递归写法
+    // 1.递归
     public int minClimb(int[] cost) {
-
         int len = cost.length;
         return Math.min(helper(cost, len - 2), helper(cost, len - 1));
     }
@@ -23,9 +23,10 @@ public class _1stairCost {
     }
 
 
-    // 使用缓存的递归，避免重复计算
-    public int minclimb(int[] cost) {
+    
 
+    // 2.使用缓存的递归，避免重复计算
+    public int minclimb(int[] cost) {
         int len = cost.length;
         int[] dp = new int[len];
         helper(cost, len - 1, dp);
@@ -46,7 +47,7 @@ public class _1stairCost {
 
 
 
-    // 空间复杂度O(n)的迭代
+    // 3.空间复杂度O(n)的迭代
     public int mincLimb(int[] cost) {
 
         int len = cost.length;
@@ -62,8 +63,8 @@ public class _1stairCost {
 
 
 
-    // 空间复杂度O(1)的迭代,最佳方法
-    // 在求每个 f(i)的时候，需要保存之前的 f(i-1)和 f(i-2)的结果，因此只要一个长度为 2 的数组即可。
+    // 4.空间复杂度O(1)的迭代,最佳方法
+    // 在求每个 f(i)的时候，需要保存之前的 f(i-1)和 f(i-2)的结果，因此只要一个长度为 2 的数组即可
     public int minc1imb(int[] cost) {
 
         int[] dp = new int[]{cost[0], cost[1]};
