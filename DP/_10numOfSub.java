f499e57441fd0316e5c33c4273d66600bb8aba08
Hermonious <92699508+hermonious@users.noreply.github.com>
2025-10-24 10:39:05 +0800

    Update _10numOfSub.java

--- DP/_10numOfSub.java
+++ DP/_10numOfSub.java
@@ -2,38 +2,47 @@
 
 
 
-// 输入字符串 S 和 T，请计算字符串 S 中有多少个子序列等于字符串 T。
-// 例如，在字符串"appplep"中，有 3 个子序列等于字符串"apple"
+ // 输入字符串 s 和 t，请计算字符串 s 中有多少个子序列等于字符串 t。
+ // 例如，在字符串 "appplep" 中，有 3 个子序列等于字符串 "apple"。
 
 
-
-public class _10numOfSub {
-    
-
-
-    // 用 f(i, j)表示字符串 S 下标从 0 到 i 的子字符串（记为 S[0..i]）中 = 字符串 T 下标从 0 到 j 的子字符串（记为 T[0..j]）的子序列的数目
-    // i < j时， f(i, j) = 0
-    
-
-    // 如果字符串 S 中下标为 i 的字符（记为 S[i]） = 字符串 T 中下标为 j 的字符（记为 T[j]）
-    // 当 S[i] = T[j] 时，f(i, j) = f(i - 1, j - 1) + f(i - 1, j)
-    // 当 S[i] != T[j] 时，f(i, j) = f(i - 1, j)
-
-    // S,T都为空时，f(-1, -1) = 1
-    // S为空时，f(-1, j) = 0
-    // T为空时，f(i, -1) = 1
-
-
-    public int distinct(String s, String t) {
+ /* 动态规划定义：
+ *   用 f(i, j) 表示字符串 s 的前 i 个字符（s[0..i-1]）中，
+ *   等于字符串 t 的前 j 个字符（t[0..j-1]）的子序列的数目。
+ *
+ * 边界条件：
+ *   - 当 t 为空（j = 0）时，f(i, 0) = 1（空字符串是任意字符串的子序列）
+ *   - 当 s 为空而 t 非空（i = 0, j > 0）时，f(0, j) = 0
+ *   - f(0, 0) = 1（两个空字符串匹配）
+ *
+ * 状态转移方程：
+ *   - 若 s[i-1] == t[j-1]：
+ *         f(i, j) = f(i-1, j-1) + f(i-1, j)
+ *     （可以选择匹配当前字符，或跳过 s[i-1]）
+ *   - 若 s[i-1] != t[j-1]：
+ *         f(i, j) = f(i-1, j)
+ *     （只能跳过 s[i-1]）
+ *
+ * 注意：虽然题目保证最终结果在 32 位有符号整数范围内，
+ *       但中间计算过程可能溢出 int，因此使用 long 类型进行 DP。
+ */
+class Solution {
+    public int numDistinct(String s, String t) {
+        
         int len1 = s.length();
         int len2 = t.length();
 
-        int[][] dp = new int[len1 + 1][len2 + 1];
+        // dp[i][j] 表示 s[0..i-1] 中包含 t[0..j-1] 作为子序列的个数
+        long[][] dp = new long[len1 + 1][len2 + 1];
         dp[0][0] = 1;
 
+        // 初始化：t 为空时，所有 s[0..i-1] 都有 1 个匹配（空子序列）
         for (int i = 0; i < len1; i++) {
             dp[i + 1][0] = 1;
+        }
 
+        // 填充 DP 表
+        for (int i = 0; i < len1; i++) {
             for (int j = 0; j < len2; j++) {
                 if (s.charAt(i) == t.charAt(j)) {
                     dp[i + 1][j + 1] = dp[i][j + 1] + dp[i][j];
@@ -42,32 +51,29 @@ public class _10numOfSub {
                 }
             }
         }
-
-        return dp[len1][len2];
+        return (int) dp[len1][len2];
     }
 
-
-
-
-
-
-
-
-    // 优化空间效率
-    public int distincT(String s, String t) {
+    /**
+     * 空间优化版本：使用一维 DP 数组。
+     * 由于 dp[i+1][j+1] 只依赖于上一行的 dp[i][j] 和 dp[i][j+1]，
+     * 可以从后往前更新 j，避免覆盖尚未使用的值。
+     */
+    public int distinctOptimized(String s, String t) {
         int len1 = s.length();
         int len2 = t.length();
 
-        int[] dp = new int[len2 + 1];
-        dp[0] = 1;
+        long[] dp = new long[len2 + 1];
+        dp[0] = 1; // t 为空的情况
 
         for (int i = 0; i < len1; i++) {
+            // 从后往前遍历 j，确保 dp[j] 是上一轮（i-1）的值
             for (int j = Math.min(len2 - 1, i); j >= 0; j--) {
                 if (s.charAt(i) == t.charAt(j)) {
                     dp[j + 1] += dp[j];
                 }
             }
         }
-        return dp[len2];
+        return (int) dp[len2];
     }
 }
