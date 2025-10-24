60018c784fcbcef8c0cc71daa5f35758e7b16b8c
Hermonious <92699508+hermonious@users.noreply.github.com>
2025-10-24 11:46:40 +0800

    Update _14partitionEqual.java

--- DP/_14partitionEqual.java
+++ DP/_14partitionEqual.java
@@ -8,12 +8,7 @@
 // 如果输入数组为[1, 2, 3, 5]，则不能将这些数字分成和相等的两部分，因此输出 false
 
 
-
-
-public class _14partitionEqual {
-    
-
-    // 函数 f(i, j)表示能否从前 i 个物品（物品标号分别为 0,1,…,i-1）中选择若干物品放满容量为 j 的背包
+// 函数 f(i, j)表示能否从前 i 个物品（物品标号分别为 0,1,…,i-1）中选择若干物品放满容量为 j 的背包
     // 判断能否从前 i 个物品中选择若干装满容量为 j 的书包时，需要考虑两种情况：
     // 1. 不选择第 i - 1 个物品，如果能从前 i-1 个物品中选择若干装满容量为 j 的书包，即：f(i - 1, j) = true，那么f(i, j) = true
     // 2. 选择第 i - 1 个物品，此时背包的容量为 j-weight[i - 1], 即：f(i - 1, j - weight[i - 1]) = true，那么f(i, j) = true
@@ -22,14 +17,18 @@ public class _14partitionEqual {
     // i = 0时， 肯定装不满，所以f(0, j) = false
 
 
+public class _14partitionEqual {
     public boolean partition(int[] nums) {
+        
         int sum = 0;
         for (int num : nums) {
             sum += num;
         }
+        
         if (sum % 2 == 1) {
             return false;
         }
+        
         return subsetSum(nums, sum / 2);
     }
 
@@ -63,26 +62,26 @@ public class _14partitionEqual {
 
 
     // 迭代代码
-    private boolean partition1(int[] nums, int target) {
+    // private boolean partition1(int[] nums, int target) {
 
-        boolean[][] dp = new boolean[nums.length + 1][target + 1];
+    //     boolean[][] dp = new boolean[nums.length + 1][target + 1];
 
-        for (int i = 0; i <= nums.length; i++) {
-            dp[i][0] = true;
-        }
+    //     for (int i = 0; i <= nums.length; i++) {
+    //         dp[i][0] = true;
+    //     }
 
-        for (int i = 1; i <= nums.length; i++) {
-            for (int j = 1; j <= target; j++) {
-                dp[i][j] = dp[i - 1][j];
+    //     for (int i = 1; i <= nums.length; i++) {
+    //         for (int j = 1; j <= target; j++) {
+    //             dp[i][j] = dp[i - 1][j];
 
-                if (!dp[i][j] && j >= nums[i - 1]) {
-                    dp[i][j] = dp[i - 1][j - nums[i - 1]];
-                }
-            }
-        }
+    //             if (!dp[i][j] && j >= nums[i - 1]) {
+    //                 dp[i][j] = dp[i - 1][j - nums[i - 1]];
+    //             }
+    //         }
+    //     }
 
-        return dp[nums.length][target];
-    }
+    //     return dp[nums.length][target];
+    // }
 
 
 
@@ -91,20 +90,19 @@ public class _14partitionEqual {
 
 
     // 优化空间效率
-    private boolean partitioN(int[] nums, int target) {
+    // private boolean partitioN(int[] nums, int target) {
 
-        boolean[] dp = new boolean[target + 1];
-        dp[0] = true;
+    //     boolean[] dp = new boolean[target + 1];
+    //     dp[0] = true;
 
-        for (int i = 1; i <= nums.length; i++) {
-            for (int j = target; j > 0; j--) {
+    //     for (int i = 1; i <= nums.length; i++) {
+    //         for (int j = target; j > 0; j--) {
                 
-                if (!dp[j] && j >= nums[i - 1]) {
-                    dp[j] = dp[j - nums[i - 1]];
-                }
-            }
-        }
-
-        return dp[target];
-    }
+    //             if (!dp[j] && j >= nums[i - 1]) {
+    //                 dp[j] = dp[j - nums[i - 1]];
+    //             }
+    //         }
+    //     }
+    //     return dp[target];
+    // }
 }
