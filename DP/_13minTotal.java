5b34912c7446249bcce836ba76df9ba3ee7c1d1c
Hermonious <92699508+hermonious@users.noreply.github.com>
2025-10-24 11:40:30 +0800

    Update _13minTotal.java

--- DP/_13minTotal.java
+++ DP/_13minTotal.java
@@ -1,86 +1,88 @@
+
+
+
+
 import java.util.List;
 
-
-
-// 在一个由数字组成的三角形中，第 1 行有 1 个数字，第 2 行有 2 个数字，以此类推，第 n 行有 n 个数字。
-// 例如，图 14.9 是一个包含 4 行数字的三角形。如果每步只能前往下一行中相邻的数字，请计算从三角形顶部到底部的路径经过的数字之和的最小值
-
-
-
-public class _13minTotal {
-    
-
-
-    // 可以移动三角形每行的位置使它们左端对齐，如图 14.10 所示。对齐之后就能很方便地用矩阵的行坐标和列坐标来定位每个数字。
-    // 如果三角形有 n 行数字，将这些行左端对齐之后就成了一个 n×n 的矩阵的左下半部分。
-    // 如果三角形中某个数字在矩阵中的行号和列号分别是 i 和 j，那么 i≥j
-
-    // 用 f(i, j)表示从三角形的顶部出发到达行号和列号分别为 i 和 j（i≥j）的位置时路径数字之和的最小值
-    // 用 T[i][j]表示三角形行号和列号分别为 i 和 j 的数字
-
-
-    // j = 0时，f(i, 0) = f(i - 1, 0) + T[i][0]
-    // j = i时，f(i, i) = f(i - 1, i - 1) + T[i][i]
-    // 其他情况，f(i, j) = min(f(i - 1, j - 1), f(i - 1, j)) + T[i][j]
-
-
-    public int minTotal(List<List<Integer>> t) {
-
-        int size = t.size();
-        int[][] dp = new int[size][size];
+/**
+ * LeetCode 120. 三角形最小路径和（Triangle）
+ * 
+ * 给定一个三角形 triangle，找出自顶向下的最小路径和。
+ * 每一步只能移动到下一行中相邻的节点上。
+ * 相邻的节点在这里指的是下标与上一层节点下标相同或者加一的两个节点。
+ * 
+ * 动态规划解法（自顶向下）：
+ * - 状态定义：dp[i][j] 表示从顶部到达第 i 行第 j 列位置的最小路径和
+ * - 状态转移：
+ *     • 若 j == 0（行首）：只能从上一行同列转移 → dp[i][j] = dp[i-1][j] + triangle[i][j]
+ *     • 若 j == i（行尾）：只能从上一行前一列转移 → dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+ *     • 否则：dp[i][j] = min(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+ * - 初始状态：dp[0][0] = triangle[0][0]
+ * - 结果：最后一行中的最小值
+ * 
+ * 时间复杂度：O(n²)，空间复杂度：O(n²)，其中 n 为三角形行数
+ */
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
         
-
-        for (int i = 0; i < size; i++) {
+        int n = triangle.size();
+        // 创建 DP 表，dp[i][j] 仅在 j <= i 时有效
+        int[][] dp = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
-
-                dp[i][j] = t.get(i).get(j);
-
-                if (i > 0 && j == 0) {
-                    dp[i][j] += dp[i - 1][j];
-                } else if (i > 0 && j == i) {
-                    dp[i][j] += dp[i - 1][j - 1];
-                } else if (i > 0) {
-                    dp[i][j] += Math.min(dp[i - 1][j - 1], dp[i - 1][j]);
+                int currentValue = triangle.get(i).get(j);
+                
+                if (i == 0) {
+                    // 起点
+                    dp[i][j] = currentValue;
+                } else if (j == 0) {
+                    // 行首：只能从上方来
+                    dp[i][j] = dp[i - 1][j] + currentValue;
+                } else if (j == i) {
+                    // 行尾：只能从左上方来
+                    dp[i][j] = dp[i - 1][j - 1] + currentValue;
+                } else {
+                    // 中间位置：取上方和左上方的较小值
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j]) + currentValue;
                 }
             }
         }
-
-        int min = Integer.MAX_VALUE;
-        for (int num : dp[size - 1]) {
-            min = Math.min(min, num);
+        
+        // 在最后一行中找最小路径和
+        int minPathSum = dp[n - 1][0];
+        for (int j = 1; j < n; j++) {
+            minPathSum = Math.min(minPathSum, dp[n - 1][j]);
         }
-
-        return min;
+        
+        return minPathSum;
     }
-
-
+}
 
 
 
     //优化空间效率
-    public int minTotal2(List<List<Integer>> t) {
+    // public int minTotal2(List<List<Integer>> t) {
 
-        int size = t.size();
-        int[] dp = new int[size];
+    //     int size = t.size();
+    //     int[] dp = new int[size];
 
-        for (List<Integer> row : t) {
-            for (int j = row.size() - 1; j >= 0; j--) {
+    //     for (List<Integer> row : t) {
+    //         for (int j = row.size() - 1; j >= 0; j--) {
 
-                if (j == 0) {
-                    dp[j] += row.get(j);
-                } else if (j == row.size() - 1) {
-                    dp[j] = dp[j - 1] + row.get(j);
-                } else {
-                    dp[j] = Math.min(dp[j - 1], dp[j]) + row.get(j);
-                }
-            }
-        }
+    //             if (j == 0) {
+    //                 dp[j] += row.get(j);
+    //             } else if (j == row.size() - 1) {
+    //                 dp[j] = dp[j - 1] + row.get(j);
+    //             } else {
+    //                 dp[j] = Math.min(dp[j - 1], dp[j]) + row.get(j);
+    //             }
+    //         }
+    //     }
+    //     int min = Integer.MAX_VALUE;
+    //     for (int num : dp) {
+    //         min = Math.min(min, num);
+    //     }
+    //     return min;
+    // }
 
-        int min = Integer.MAX_VALUE;
-        for (int num : dp) {
-            min = Math.min(min, num);
-        }
-
-        return min;
-    }
-}
